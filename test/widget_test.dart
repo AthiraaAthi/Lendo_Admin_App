1fdd9cf82e72c533b7943eac9b244761dd1a54df
AthiraaAthi <athiraa2612@gmail.com>
2025-02-07 20:11:04 +0530

    Rename MyApp to LendoAdmin and update widget references

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:lendo_admin/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const LendoAdmin());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
