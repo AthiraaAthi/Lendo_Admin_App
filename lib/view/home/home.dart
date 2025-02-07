f4f56553dec3ee0e1cdd08dd96a6c713e9e38a4f
AthiraaAthi <athiraa2612@gmail.com>
2025-02-07 20:37:24 +0530

    Add end drawer to HomeScreen and refactor constructor

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:lendo_admin/utils/color_constant/color_constant.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
+  HomeScreen({super.key});
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      endDrawer: Drawer(),
       appBar: AppBar(
         backgroundColor: mainBlue,
         title: Text('Lendo Help Desk',
