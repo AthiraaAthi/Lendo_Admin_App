fc8184a6600323852b3a70ee8d23dee32fb4da34
AthiraaAthi <athiraa2612@gmail.com>
2025-02-07 20:12:15 +0530

    Disable debug banner in MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,8 @@ class LendoAdmin extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
