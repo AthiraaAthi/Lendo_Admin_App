f8b74dc699ee0d9014a472a8763fdf3e34b935f3
AthiraaAthi <athiraa2612@gmail.com>
2025-02-08 20:06:09 +0530

    added fontweight to text

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -29,7 +29,7 @@ class HomeScreen extends StatelessWidget {
               children: [
                 Text(
                   "Accepted files",
-                  style: TextStyle(fontSize: 17),
+                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600),
                 ),
                 Icon(Icons.arrow_right)
               ],
