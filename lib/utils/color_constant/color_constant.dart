49ccb4c13c4d39980590de2808274bfa3d1d0325
AthiraaAthi <athiraa2612@gmail.com>
2025-02-13 17:33:07 +0530

    update fillColor in TextField for SelectedPerson widget to enhance visual consistency

--- lib/utils/color_constant/color_constant.dart
+++ lib/utils/color_constant/color_constant.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const Color mainBlue = Color(0xff1D13D9);
 const Color white = Colors.white;
+const Color lightBlue = Color.fromARGB(255, 221, 224, 225);
--- lib/view/home/selected.dart
+++ lib/view/home/selected.dart
@@ -33,7 +33,7 @@ class SelectedPerson extends StatelessWidget {
                   hintStyle:
                       TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
                   filled: true,
-                  fillColor: const Color.fromARGB(255, 232, 230, 230),
+                  fillColor: const Color.fromARGB(255, 221, 224, 225),
                   border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(8),
                       borderSide: BorderSide.none)),
@@ -47,7 +47,7 @@ class SelectedPerson extends StatelessWidget {
                   hintStyle:
                       TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
                   filled: true,
-                  fillColor: const Color.fromARGB(255, 232, 230, 230),
+                  fillColor: const Color.fromARGB(255, 221, 224, 225),
                   border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(8),
                       borderSide: BorderSide.none)),
@@ -62,7 +62,7 @@ class SelectedPerson extends StatelessWidget {
                   hintStyle:
                       TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
                   filled: true,
-                  fillColor: const Color.fromARGB(255, 232, 230, 230),
+                  fillColor: const Color.fromARGB(255, 221, 224, 225),
                   border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(8),
                       borderSide: BorderSide.none)),
