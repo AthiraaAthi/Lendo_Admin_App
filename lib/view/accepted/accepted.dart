674d03f3180148fcf2884eb444838919a9df3807
AthiraaAthi <athiraa2612@gmail.com>
2025-02-20 19:29:24 +0530

    added AcceptedFiles widget for managing accepted views

---
+++ lib/view/accepted/accepted.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AcceptedFiles extends StatelessWidget {
+  const AcceptedFiles({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
