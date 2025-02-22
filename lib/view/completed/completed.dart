479786a049e867cf7388faf4d71a1e72e397e7c3
AthiraaAthi <athiraa2612@gmail.com>
2025-02-22 17:47:57 +0530

    added AppBar with mainBlue background to CompletedFiles widget

--- lib/view/completed/completed.dart
+++ lib/view/completed/completed.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:lendo_admin/utils/color_constant/color_constant.dart';
 
 class CompletedFiles extends StatelessWidget {
   const CompletedFiles({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mainBlue,
+      ),
+    );
   }
 }
