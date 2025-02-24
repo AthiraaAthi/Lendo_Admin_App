923179557e59433322f617d9fc0659f8d24f061f
AthiraaAthi <athiraa2612@gmail.com>
2025-02-24 17:05:00 +0530

    add AcceptedSelectedScreen widget with placeholder implementation

--- lib/view/accepted/accepted_selected.dart
+++ lib/view/accepted/accepted_selected.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class AcceptedSelectedScreen extends StatelessWidget {
+  const AcceptedSelectedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
