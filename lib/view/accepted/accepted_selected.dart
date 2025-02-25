e36d2adc4b48b46f6755eeb9dbfc469f646ac794
AthiraaAthi <athiraa2612@gmail.com>
2025-02-25 18:55:39 +0530

    added SizedBox with height to AcceptedSelectedScreen for improved layout spacing

--- lib/view/accepted/accepted_selected.dart
+++ lib/view/accepted/accepted_selected.dart
@@ -65,6 +65,9 @@ class AcceptedSelectedScreen extends StatelessWidget {
                       borderRadius: BorderRadius.circular(8),
                       borderSide: BorderSide.none)),
             ),
+            SizedBox(
+              height: 100,
+            ),
           ],
         ),
       ),
