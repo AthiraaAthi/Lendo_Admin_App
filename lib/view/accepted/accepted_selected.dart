74e10c31b49e08bf35adba5068d7f8cfbc56892d
AthiraaAthi <athiraa2612@gmail.com>
2025-02-25 18:43:30 +0530

    added vertical spacing with SizedBox in AcceptedSelectedScreen for improved layout

--- lib/view/accepted/accepted_selected.dart
+++ lib/view/accepted/accepted_selected.dart
@@ -36,6 +36,9 @@ class AcceptedSelectedScreen extends StatelessWidget {
                       borderRadius: BorderRadius.circular(8),
                       borderSide: BorderSide.none)),
             ),
+            SizedBox(
+              height: 15,
+            ),
           ],
         ),
       ),
