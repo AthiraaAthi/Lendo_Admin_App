fabd299af77cc572b7efb46525b8e859a405a77b
AthiraaAthi <athiraa2612@gmail.com>
2025-02-27 17:49:10 +0530

    added spacing between elements in CompletedSelectedScreen for improved layout

--- lib/view/completed/completed_selected.dart
+++ lib/view/completed/completed_selected.dart
@@ -50,6 +50,9 @@ class CompletedSelectedScreen extends StatelessWidget {
                       borderRadius: BorderRadius.circular(8),
                       borderSide: BorderSide.none)),
             ),
+            SizedBox(
+              height: 15,
+            ),
           ],
         ),
       ),
