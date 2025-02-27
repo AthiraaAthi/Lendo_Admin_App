abdf4a8523bb86058e599167ff75c28f36f4239e
AthiraaAthi <athiraa2612@gmail.com>
2025-02-27 17:46:03 +0530

    added hint text to TextField in CompletedSelectedScreen for better user guidance

--- lib/view/completed/completed_selected.dart
+++ lib/view/completed/completed_selected.dart
@@ -40,7 +40,9 @@ class CompletedSelectedScreen extends StatelessWidget {
               height: 15,
             ),
             TextField(
-              decoration: InputDecoration(),
+              decoration: InputDecoration(
+                hintText: "Phone Number:",
+              ),
             ),
           ],
         ),
